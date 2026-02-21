fff3bfe75bd972ad695492de662e85a40b817a5f
jolovicdev <dusan.jolovic@proton.me>
2026-02-21 21:54:31 +0100

    chore: bump version to 3.0.1

--- shandu/__init__.py
+++ shandu/__init__.py
@@ -1,6 +1,6 @@
 from .contracts import AISearchResult, ResearchRequest, ResearchRunResult, RunEvent
 from .engine import ShanduEngine
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 __all__ = ["AISearchResult", "ResearchRequest", "ResearchRunResult", "RunEvent", "ShanduEngine"]

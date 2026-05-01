2a1f7e05b00c07441271b321ff1b06f7203de4b9
Dusan Jolovic <dusan.jolovic@proton.me>
2026-05-02 00:23:32 +0200

    chore: bump version 3.0.6 -> 3.0.7, add missing lxml dependency

--- shandu/__init__.py
+++ shandu/__init__.py
@@ -1,6 +1,6 @@
 from .contracts import AISearchResult, ResearchRequest, ResearchRunResult, RunEvent
 from .engine import ShanduEngine
 
-__version__ = "3.0.6"
+__version__ = "3.0.7"
 
 __all__ = ["AISearchResult", "ResearchRequest", "ResearchRunResult", "RunEvent", "ShanduEngine"]
